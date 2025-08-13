cd68b2bf9b21ce5d618e2f0e3ab725b01a0048e1
Cliente CONTROL-PC <CltControl@GGIECLUGWRK164.espol.edu.ec>
2025-08-13 10:13:08 -0500

    chore: login system

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -130,9 +130,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 API_URL = 'http://brun025.pythonanywhere.com/landing/api/index/?format=json'
 
-ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost:8000",
+    "http://127.0.0.1:8000"
+    ]
 
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000"]
+ALLOWED_HOSTS = [
+    "*",
+]
 
 # Fallo: acceso sin autenticación
 LOGIN_URL = '/login/'
